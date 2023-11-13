b1413364d600b47f9b615538bd802fa90073298c
alberto diaz <albersan338@gmail.com>
2023-11-13 16:04:07 -0500

    Implementación de los Hooks useState, useEffect y la persistencia de datos localStorage

--- my-react-task-list/src/components/Task.jsx
+++ my-react-task-list/src/components/Task.jsx
@@ -1,17 +1,62 @@
-import { FaEdit, FaTrashAlt, FaCheck } from "react-icons/fa";
+import React, { useState } from "react";
 
-const Task = ({ name, status }) => {
+const Task = ({ task, onDelete, onToggleStatus, onEdit }) => {
+  // Estado para controlar si la tarea está en modo de edición
+  const [isEditing, setEditing] = useState(false);
+  // Estado para almacenar los cambios en el título y la descripción durante la edición
+  const [editedTask, setEditedTask] = useState({
+    title: task.title,
+    description: task.description,
+  });
+
+  // Manejar el inicio de la edición
+  const handleEdit = () => {
+    setEditing(true);
+  };
+
+  // Manejar el guardado de los cambios durante la edición
+  const handleSave = () => {
+    onEdit(editedTask);
+    setEditing(false);
+  };
+
   return (
-    <div style={{ display: "flex" }}>
-      <FaCheck />
-      <li style={{ listStyle: "none" }}>
-        {name} - {status ? "Completed" : "Incomplete"}
-      </li>
-      <div style={{ margin: "16px 0 0 8px", order: 2, cursor: "pointer" }}>
-        <FaEdit />
-        <FaTrashAlt />
-      </div>
-    </div>
+    <li>
+      {isEditing ? (
+        // Formulario de edición durante la edición
+        <div>
+          <input
+            type="text"
+            value={editedTask.title}
+            onChange={(e) =>
+              setEditedTask({ ...editedTask, title: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            value={editedTask.description}
+            onChange={(e) =>
+              setEditedTask({ ...editedTask, description: e.target.value })
+            }
+          />
+          <button onClick={handleSave}>Guardar</button>
+        </div>
+      ) : (
+        // Vista normal de la tarea
+        <div>
+          <span>
+            {task.title} - {task.description} -{" "}
+            {task.status ? "Completada" : "Pendiente"}
+          </span>
+          {/* Botones para marcar como completada, editar y eliminar una tarea */}
+          <button onClick={onToggleStatus}>
+            {task.status ? "Marcar como Pendiente" : "Marcar como Completada"}
+          </button>
+          <button onClick={handleEdit}>Editar</button>
+          <button onClick={onDelete}>Eliminar</button>
+        </div>
+      )}
+    </li>
   );
 };
 
--- my-react-task-list/src/components/TaskList.jsx
+++ my-react-task-list/src/components/TaskList.jsx
@@ -1,18 +1,94 @@
+import React, { useState, useEffect } from "react";
 import Task from "./Task";
-import { FaPlus } from "react-icons/fa";
 
-const TaskList = ({ tasks }) => {
+const TaskList = () => {
+  // Estado para almacenar la lista de tareas
+  const [tasks, setTasks] = useState([]);
+  // Estados para los campos de nueva tarea
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+  const [newTaskDescription, setNewTaskDescription] = useState("");
+
+  // useEffect para cargar las tareas desde localStorage al inicio
+  useEffect(() => {
+    const storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    setTasks(storedTasks);
+  }, []);
+
+  // Función para agregar una nueva tarea
+  // trim() me sirve para eliminar los espacion en blancos (al inicio y al final)
+  const addTask = () => {
+    if (newTaskTitle.trim() !== "") {
+      const newTask = {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        description: newTaskDescription,
+        status: false,
+      };
+
+      const updatedTasks = [...tasks, newTask];
+      setTasks(updatedTasks);
+      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+
+      // Limpiar campos después de agregar tarea
+      setNewTaskTitle("");
+      setNewTaskDescription("");
+    }
+  };
+
+  // Función para eliminar una tarea
+  const deleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+  };
+
+  // Función para cambiar el estado de una tarea entre completada y pendiente
+  const toggleTaskStatus = (taskId) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, status: !task.status } : task
+    );
+    setTasks(updatedTasks);
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+  };
+
+  // Función para editar una tarea
+  const editTask = (taskId, updatedTask) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, ...updatedTask } : task
+    );
+    setTasks(updatedTasks);
+    localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+  };
+
   return (
     <div>
-      <form style={{ display: "flex" }}>
-        <input type="text" placeholder="Agregar nueva tarea" />
-        <button type="submit">
-          <FaPlus />
-        </button>
-      </form>
-      <ul>
+      <h2>Task List</h2>
+      <div>
+        {/* Formulario para agregar nueva tarea */}
+        <label>Title:</label>
+        <input
+          type="text"
+          value={newTaskTitle}
+          onChange={(e) => setNewTaskTitle(e.target.value)}
+        />
+        <label>Description:</label>
+        <input
+          type="text"
+          value={newTaskDescription}
+          onChange={(e) => setNewTaskDescription(e.target.value)}
+        />
+        <button onClick={addTask}>Agregar</button>
+      </div>
+      <ul style={{ listStyle: "none" }}>
+        {/* Iterar sobre las tareas y renderizar el componente Task para cada una */}
         {tasks.map((task) => (
-          <Task key={task.id} name={task.name} status={task.status} />
+          <Task
+            key={task.id}
+            task={task}
+            onDelete={() => deleteTask(task.id)}
+            onToggleStatus={() => toggleTaskStatus(task.id)}
+            onEdit={(updatedTask) => editTask(task.id, updatedTask)}
+          />
         ))}
       </ul>
     </div>
